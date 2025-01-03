bf733b7644b62bc391e3d5bddcbff7d753c1cfc8
Nicolas <nicolashorne99@gmail.com>
2025-01-03 13:43:13 -0500

    nav compament not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
+import React from 'react'
+
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/about";
 import Contact from "./pages/Contact";
+import Nav from "./components/Nav"
 
 function App() {
   return (
-    <div>
       <Router>
+        <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
-    </div>
   );
 }
 
