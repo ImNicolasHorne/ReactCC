d0dd45a64c2c1a667717617cb1469b4e5663706e
Nicolas <nicolashorne99@gmail.com>
2025-01-03 14:47:38 -0500

    routes unclickable

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/about";
 import Contact from "./pages/Contact";
 import Nav from "./components/Nav"
+import Users from "./pages/Users"
 
 function App() {
   return (
-      <Router>
+      <>
         <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/users/:name" element={<Users />} />
         </Routes>
-      </Router>
+      </>
   );
 }
 
---
+++ src/pages/Users.jsx
@@ -0,0 +1,5 @@
+function Users(){
+    return <h1>Users Bitch!</h1>
+}
+
+export default Users
